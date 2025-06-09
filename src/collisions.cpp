ea39efc87555cb384cb6a96a9cf544181afa60b3
ryanwebb24 <ryanwebb2444@gmail.com>
2025-06-09 15:33:27 -0600

    add loops

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -5,4 +5,16 @@
 
 void getCollisions(std::vector<Particle> &particles)
 {
+
+    for (size_t i = 0; i < particles.size(); ++i)
+    {
+        for (size_t j = i + 1; j < particles.size(); ++j)
+        {
+            if (checkCollision(particles[i], particles[j]))
+            {
+                // Basic elastic bounce (we’ll improve this later)
+                resolveCollision(particles[i], particles[j]);
+            }
+        }
+    }
 }
